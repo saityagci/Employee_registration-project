4de7f3e7d6455581a9072766f3b66c76183abf84
saityagci <saityagci@gmail.com>
2022-09-27 21:37:40 -0400

    employee registration html and DataGenerator

--- src/main/java/com/cydeo/controller/EmployeeController.java
+++ src/main/java/com/cydeo/controller/EmployeeController.java
@@ -1,6 +1,9 @@
 package com.cydeo.controller;
 
+import com.cydeo.bootstrap.DataGenerator;
+import com.cydeo.model.Employee;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,7 +11,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/employee")
 public class EmployeeController {
     @GetMapping("/register")
-    public String createEmployee(){
+    public String createEmployee(Model model){
+        model.addAttribute("employee",new Employee());
+        model.addAttribute("states", DataGenerator.getAllStates());
+
         return "/employee/employee-create";
     }
 }
