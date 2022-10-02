4159ee30eb454b65e1b0c33545ebef6b533e2af4
saityagci <saityagci@gmail.com>
2022-10-02 01:13:55 -0400

    valid annotations

--- src/main/java/com/cydeo/controller/EmployeeController.java
+++ src/main/java/com/cydeo/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import com.cydeo.bootstrap.DataGenerator;
 import com.cydeo.model.Employee;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,7 +23,11 @@ public class EmployeeController {
         return "/employee/employee-create";
     }
     @PostMapping("/list")
-    public String employeeList(@Valid @ModelAttribute("employee") Employee employee, Model model){
+    public String employeeList(@Valid @ModelAttribute("employee") Employee employee, BindingResult bindingResult, Model model){
+        if (bindingResult.hasErrors()){
+            model.addAttribute("states", DataGenerator.getAllStates());
+            return "employee/employee-create";
+        }
         DataGenerator.saveEmployee(employee);
         model.addAttribute("employees",DataGenerator.readAllEmployee());
 
